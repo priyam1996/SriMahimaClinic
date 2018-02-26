81f9f2e633ed994fb52be86678ce0b6b179d83e1
priyam1996 <priyankareddythummala@gmail.com>
2018-02-26 14:47:40 +0530

    preliminarydata.xml

---
+++ app/src/main/java/com/kuncham/cont/srimahimaclinic/PreliminaryData.java
@@ -0,0 +1,13 @@
+package com.kuncham.cont.srimahimaclinic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PreliminaryData extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_preliminary_data);
+    }
+}
