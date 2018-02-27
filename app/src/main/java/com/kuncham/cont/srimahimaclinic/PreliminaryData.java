b164d7684ed45f19622d03520fdd4f47072be70a
priyam1996 <priyankareddythummala@gmail.com>
2018-02-27 18:08:41 +0530

    preliminarydata

--- app/src/main/java/com/kuncham/cont/srimahimaclinic/PreliminaryData.java
+++ app/src/main/java/com/kuncham/cont/srimahimaclinic/PreliminaryData.java
@@ -4,17 +4,13 @@ import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
-import android.widget.Toast;
 
 import java.util.Calendar;
 
 public class PreliminaryData extends AppCompatActivity {
 
-    DatePicker simpleDatePicker;
-    Button submit;
 
     EditText date;
     DatePickerDialog datePickerDialog;
